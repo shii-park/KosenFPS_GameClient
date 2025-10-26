f585cd1edec64006261e778ea05e1a36ca06f825
woods-one <0820ricer@gmail.com>
2025-10-27 05:31:47 +0900

    検証：終了することも確認

--- Assets/Main/OutGame/Scripts/SelectButton.cs
+++ Assets/Main/OutGame/Scripts/SelectButton.cs
@@ -6,11 +6,11 @@ public class SelectButton : MonoBehaviour
     [SerializeField]
     private UnityEvent _event;
     
-    private AimCore _aimCore = AimCore.Instance;
+    private AimCore _aimCore => AimCore.Instance;
     
     void Update()
     {
-        if (this.gameObject.GetComponent<RectTransform>().IsOverlapping(_aimCore.AimTransform))
+        if (this.gameObject.GetComponent<RectTransform>().IsOverlapping(AimCore.Instance.AimTransform))
         {
             _event.Invoke();
         }
