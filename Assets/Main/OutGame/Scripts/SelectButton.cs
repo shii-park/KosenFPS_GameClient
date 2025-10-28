ba4a5de72d14eeb6c6268ca69d33c85acdf6a780
woods-one <0820ricer@gmail.com>
2025-10-29 07:03:21 +0900

    完成：OutGameのUI完成

--- Assets/Main/OutGame/Scripts/SelectButton.cs
+++ Assets/Main/OutGame/Scripts/SelectButton.cs
@@ -2,11 +2,15 @@ using System;
 using R3;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class SelectButton : MonoBehaviour
 {
     [SerializeField]
     private UnityEvent _event;
+    
+    [SerializeField]
+    private Image _image;
     
     private AimCore _aimCore => AimCore.Instance;
     
@@ -26,6 +30,11 @@ public class SelectButton : MonoBehaviour
         _aimCore.IsShot.Where(_ => _isSelected.CurrentValue).Subscribe(_ =>
         {
             _event.Invoke();
+        });
+
+        _isSelected.Subscribe(isSelected =>
+        {
+            _image.enabled = isSelected;
         });
     }
 
