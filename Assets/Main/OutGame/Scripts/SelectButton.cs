bc96c0ab561228414a0490f783085d50aa9bf169
woods-one <0820ricer@gmail.com>
2025-10-29 06:02:55 +0900

    追加：選択してUIを選べるように

--- Assets/Main/OutGame/Scripts/SelectButton.cs
+++ Assets/Main/OutGame/Scripts/SelectButton.cs
@@ -1,3 +1,5 @@
+using System;
+using R3;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,11 +10,23 @@ public class SelectButton : MonoBehaviour
     
     private AimCore _aimCore => AimCore.Instance;
     
-    void Update()
+    private ReactiveProperty<bool> _isSelected = new ReactiveProperty<bool>();
+    
+    void Start()
     {
-        if (this.gameObject.GetComponent<RectTransform>().IsOverlapping(AimCore.Instance.AimTransform))
+        Observable.Interval(TimeSpan.FromSeconds(0.1)) // 0.5秒ごとにチェック
+            .Select(_ => this.gameObject.GetComponent<RectTransform>().IsOverlapping(AimCore.Instance.AimTransform)) // 任意の関数
+            .DistinctUntilChanged()
+            .Subscribe(isSelected =>
+            {
+                _isSelected.Value = isSelected;
+            })
+            .AddTo(this);
+
+        _aimCore.IsShot.Where(_ => _isSelected.CurrentValue).Subscribe(_ =>
         {
             _event.Invoke();
-        }
+        });
     }
+
 }
