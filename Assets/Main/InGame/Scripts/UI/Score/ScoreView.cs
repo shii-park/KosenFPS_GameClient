ac89b02f14398617e82cd4702a2de64cf6bda93a
woods-one <0820ricer@gmail.com>
2025-10-31 01:23:10 +0900

    追加：ScoreViewの完成

--- Assets/Main/InGame/Scripts/UI/Score/ScoreView.cs
+++ Assets/Main/InGame/Scripts/UI/Score/ScoreView.cs
@@ -1,16 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreView : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Text _text;
 
-    // Update is called once per frame
-    void Update()
+    public void SetScore(int score)
     {
-        
+        _text.text = score.ToString();
     }
 }
